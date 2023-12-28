c96598d240ab2eb9e02b72ca1da919457a89db7f
mario <mariodemartini@gmail.com>
2023-12-28 18:36:12 -0300

    :sparkles: feat: including recipes references

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'recipes/home.html', context={'name': 'Mario'})
+
+
+def about(request):
+    return HttpResponse('About Us')
+
+
+def contact(request):
+    return HttpResponse('Contact Us')
