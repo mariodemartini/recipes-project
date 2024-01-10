0b798ab154301e210ba7462bd92f5551efc6c75c
mario <mariodemartini@gmail.com>
2024-01-10 10:22:52 -0300

    :recycle: recfator: improving search method

--- recipes/views.py
+++ recipes/views.py
@@ -35,8 +35,11 @@ def recipe(request, id):
 
 
 def search(request):
-    search_term = request.GET.get('search')
+    search_term = request.GET.get('search', '').strip()
     if not search_term:
         raise Http404()
     
-    return render(request, 'recipes/pages/search.html')
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'"{search_term}" |',
+        'search_term': search_term,
+    })
