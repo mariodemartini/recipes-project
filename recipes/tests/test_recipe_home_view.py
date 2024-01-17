562c1a390c3e55af2d1ceb391e65afa153be9118
mario <mariodemartini@gmail.com>
2024-01-17 10:10:32 -0300

    :white_check_mark: test: adding new test to pagination home view

--- recipes/tests/test_recipe_home_view.py
+++ recipes/tests/test_recipe_home_view.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 from django.urls import reverse, resolve
 from recipes import views
 
@@ -23,7 +24,7 @@ class RecipeHomeViewTest(RecipeTestBase):
 
     def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
         response = self.client.get(reverse('recipes:home'))
-        self.assertIn('', response.content.decode('utf-8'))
+        self.assertIn('No recipes found here 🥲', response.content.decode('utf-8'))
 
 
     def test_recipe_home_template_loads_recipes(self):
@@ -46,4 +47,20 @@ class RecipeHomeViewTest(RecipeTestBase):
         response = self.client.get(reverse('recipes:home'))
 
         self.assertIn('', response.content.decode('utf-8'))
+
+    
+    def test_recipe_home_is_paginated(self):
+        for i in range(8):
+            kwargs = {'slug': f'r{i}', 'author_data': {'username': f'u{i}'}}
+            self.make_recipe(**kwargs)
+
+        with patch('recipes.views.PER_PAGE', new=3):
+            response = self.client.get(reverse('recipes:home'))
+            recipes = response.context['recipes']
+            paginator = recipes.paginator
+
+            self.assertEqual(paginator.num_pages, 3)
+            self.assertEqual(len(paginator.get_page(1)), 3)
+            self.assertEqual(len(paginator.get_page(2)), 3)
+            self.assertEqual(len(paginator.get_page(3)), 2)
 
