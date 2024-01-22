680d7a2a4cfe45b478f4c479c26da0b315e637f1
mario <mariodemartini@gmail.com>
2024-01-21 21:12:01 -0300

    :sparkles: feat: adding override form fields

--- authors/forms.py
+++ authors/forms.py
@@ -2,7 +2,40 @@ from django import forms
 from django.contrib.auth.models import User
 
 
+def add_placeholder(field, placeholder_val):
+    field.widget.attrs['placeholder'] = placeholder_val
+
+
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Digite seu nome de usuário')
+        add_placeholder(self.fields['email'], 'Digite seu e-mail')
+        add_placeholder(self.fields['first_name'], 'Digite seu nome')
+        add_placeholder(self.fields['last_name'], 'Digite seu sobrenome')
+
+    senha = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Senha'
+        }),
+        error_messages={
+            'required': 'Senha obrigatória'
+        },
+        help_text=(
+            'A senha deve ter pelo menos uma letra maiúscula, '
+            'uma letra minúscula e um número. '
+            'Mínimo de 8 caracteres.'
+        )
+    )
+
+    confirmar = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Confirme sua senha'
+        })
+    )
+
     class Meta:
         model = User
         fields = [
@@ -10,7 +43,30 @@ class RegisterForm(forms.ModelForm):
             'last_name',
             'username',
             'email',
-            'password',
+            # 'password',
         ]
+
+        labels = {
+            'first_name': 'Nome',
+            'last_name': 'Sobrenome',
+            'username': 'Usuário',
+            'email': 'E-mail',
+        }
+
+        help_text = {
+            'email': 'Digite um email válido.',
+        }
+
+        error_message = {
+            'username': {
+                'required': 'Campo Obrigatório',
+            }
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Digite seu nome'
+            })
+        }
 
 
