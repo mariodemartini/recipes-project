9eafbd0b52d48468598a4f87e6e53e95b96f7879
mario <mariodemartini@gmail.com>
2024-01-21 21:13:40 -0300

    :recycle: refactor: improving functions of register form

--- authors/views.py
+++ authors/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.http import Http404
 
 # Create your views here.
 def register_view(request):
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+    
+    post = request.POST
+    request.session['register_form_data'] = post
+    form = RegisterForm(post)
+
+    return redirect('authors:register')
+    # return render(request, 'authors/pages/register_view.html', {
+    #     'form': form,
+    # })
