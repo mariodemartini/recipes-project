4f778d662e842f869e275602c497e93bce47f48d
mario <mariodemartini@gmail.com>
2024-01-28 15:33:37 -0300

    :recycle: refactor: updating the includes path

--- authors/views.py
+++ authors/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 
 from django.http import Http404
 from django.contrib import messages
+from django.urls import reverse
 
 from .forms import RegisterForm
 
@@ -11,6 +12,7 @@ def register_view(request):
     form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
+        'form_action': reverse('authors:create'),
     })
 
 
