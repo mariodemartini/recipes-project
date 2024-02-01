0325e854499d82dcfbb25aabcb7bde74fd06ec6c
mario <mariodemartini@gmail.com>
2024-02-01 08:15:59 -0300

    :sparkles: feat: creating function and url to logout

--- authors/views.py
+++ authors/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from django.http import Http404
 from django.contrib import messages
 from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 from .forms import RegisterForm, LoginForm
 
@@ -29,8 +31,9 @@ def register_create(request):
         user.set_password(user.password)
         user.save()
         messages.success(request, 'Seu usuário foi criado, faça login.')
-
         del(request.session['register_form_data'])
+
+        return redirect(reverse('authors:login'))
 
     return redirect('authors:register')
 
@@ -43,6 +46,36 @@ def login_view(request):
     })
 
 def login_create(request):
-    return render(request, 'authors/pages/login.html')
+    if not request.POST:
+        raise Http404()
+    
+    form = LoginForm(request.POST)
+    login_url = reverse('authors:login')
 
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+        )
+        if authenticated_user is not None:
+            messages.success(request, 'Login válido!')
+            login(request, authenticated_user)
+        else:
+            messages.error(request, 'Usuário e/ou senha inválidos')
+    else:
+        messages.error(request, 'Usuário e/ou senha inválidos')
+
+    return redirect(login_url)
+    
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def logout_view(request):
+    if not request.POST:
+        return redirect(reverse('authors:login'))
+    
+    if request.POST.get('username') != request.user.username:
+        return redirect(reverse('authors:login'))
+    
+    logout(request)
+    return redirect(reverse('authors:login'))
 
