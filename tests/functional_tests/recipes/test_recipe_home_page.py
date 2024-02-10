35e9aa8dd357593c35e92cee4c64df7fee0ee50d
mario <mariodemartini@gmail.com>
2024-02-10 16:24:54 -0300

    :recycle: refactor: adding pytest maker functional_test

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,9 +1,10 @@
+import pytest
 from selenium.webdriver.common.by import By
 from .base import RecipeBaseFunctionalTest
 
 
+@pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
-    
     def test_recipe_home_page_whitout_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
