a4b56902b52df7f41e8d02ab0cd3ef24ce8459cf
mario <mariodemartini@gmail.com>
2024-02-14 08:33:54 -0300

    :white_check_mark: test: creating functional test to pagination

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -43,3 +43,23 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
         )
 
         
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_home_page_pagination(self):
+        self.make_recipe_in_batch()
+
+        # User opens the page
+        self.browser.get(self.live_server_url)
+
+        # See that there is pagination and click on page 2
+        page2 = self.browser.find_element(
+            By.XPATH,
+            '//a[@aria-label="Vá para a página 2"]'
+        )
+        page2.click()
+
+        # See that there are 2 more recipes on page 2
+        self.assertEqual(
+            len(self.browser.find_elements(By.CLASS_NAME, 'recipe')),
+            2
+        )
+
