39c8701da07e113564f2847a5634d6bf552b17d5
mario <mariodemartini@gmail.com>
2024-02-19 10:55:42 -0300

    :white_check_mark: test: creating tests to logout

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -66,6 +66,55 @@ class AuthorsLoginTest(AuthorsBaseTest):
 
         # Vê uma mensagem de erro na tela
         self.assertIn(
-            'Invalid credentials',
+            'Usuário e/ou senha inválidos',
             self.browser.find_element(By.TAG_NAME, 'body').text
         )
+
+    def test_user_tries_to_logout_using_get_method(self):
+        User.objects.create_user(username='my_user', password='my_pass')
+        self.client.login(username='my_user', password='my_pass')
+
+        response = self.client.get(
+            reverse('authors:logout'),
+            follow=True
+        )
+
+        self.assertIn(
+            'Invalid logout request',
+            response.content.decode('utf-8')
+        )
+
+    def test_user_tries_to_logout_another_user(self):
+        User.objects.create_user(username='my_user', password='my_pass')
+        self.client.login(username='my_user', password='my_pass')
+
+        response = self.client.post(
+            reverse('authors:logout'),
+            data={
+                'username': 'another_user'
+            },
+            follow=True
+        )
+
+        self.assertIn(
+            'Invalid logout user',
+            response.content.decode('utf-8')
+        )
+
+    def test_user_can_logout_successfully(self):
+        User.objects.create_user(username='my_user', password='my_pass')
+        self.client.login(username='my_user', password='my_pass')
+
+        response = self.client.post(
+            reverse('authors:logout'),
+            data={
+                'username': 'my_user'
+            },
+            follow=True
+        )
+
+        self.assertIn(
+            'Logged out successfully',
+            response.content.decode('utf-8')
+        )
+
